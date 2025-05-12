04ce6ac6b8419a2ce5e999ff013da13a63c47a59
Matteda Ramesh <85834506+rummy143679@users.noreply.github.com>
2025-05-12 19:07:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+  let sr = arr.sort();
+  cost += sr[0]+sr[1];
+  let nxt = sr.slice(2);
+  nxt.push(sr[0]+sr[1]);
+  if(nxt.length >= 2){
+    return mincost(nxt);
+  }else{
+      return cost;
+  }
 }
 
 module.exports=mincost;
